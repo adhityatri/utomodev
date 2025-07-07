332a33ab39aa173c180569e14a67514d85dd9dbd
Adhitya Tri <adhitya.tri@gramedia.id>
2025-07-07 16:53:04 +0700

    Update sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,38 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  ssr: true,
+  app: {
+    head: {
+        script: [
+          {
+            type: 'application/ld+json',
+            innerHTML: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'SoftwareApplication', // atau 'LocalBusiness'
+              'name': 'Utomo Solutions',
+              'address': {
+                '@type': 'PostalAddress',
+                'streetAddress': 'Jl Kasuari Blok Kk3 No 31',
+                'addressLocality': 'Jember',
+                'postalCode': '68117',
+                'addressRegion': 'Jawa Timur',
+                'addressCountry': 'ID'
+              },
+              'telephone': '+62851-2803-1998',
+              'url': 'https://utomosolutions.com',
+            }),
+          }
+        ]
+    }
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui',
     '@vueuse/nuxt',
     '@nuxtjs/device',
+    '@nuxtjs/sitemap',
   ],
   css: ['~/assets/css/main.css'],
   ui: {
