d02125f5ea1fb33acbfc88e4fe7dbe4cad203d8f
Adhitya Tri <adhityatriwahyu@gmail.com>
2026-01-01 14:02:00 +0700

    remove vercel

--- app/app.vue
+++ app/app.vue
@@ -5,13 +5,8 @@
       <app-top-bar />
       <div class="flex flex-col flex-1">
         <NuxtPage />
-        <SpeedInsights />
       </div>
       <app-footer />
     </div>
   </UApp>
 </template>
-
-<script setup lang="ts">
-  import { SpeedInsights } from "@vercel/speed-insights/nuxt";
-</script>
